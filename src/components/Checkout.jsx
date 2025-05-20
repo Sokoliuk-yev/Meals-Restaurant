bc97a8b17e620031ed09ad2f10c31a7e00850ba2
Sokoliuk Yevhen <sokoliukyevhen4@gmail.com>
2025-05-21 00:06:48 +0800

    Add files via upload

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,113 @@
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { useContext } from "react";
+import { currencyFormatter } from "../util/formatting";
+import Input from "./UI/Input";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const Checkout = () => {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const {
+    data,
+    isLoading: isSending,
+    error,
+    sendRequest,
+    clearData,
+  } = useHttp("http://localhost:3000/orders", requestConfig);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleFinish() {
+    userProgressCtx.hideCheckout();
+    cartCtx.clearCart();
+    clearData();
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const fd = new FormData(e.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    sendRequest(
+      JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      })
+    );
+  }
+
+  let actions = (
+    <>
+      <Button type="button" textOnly onClick={handleCloseCart}>
+        Close
+      </Button>
+      <Button>Submit Order</Button>
+    </>
+  );
+
+  if (isSending) {
+    actions = <span>Sending order data...</span>;
+  }
+
+  if (data && !error) {
+    return (
+      <Modal
+        open={userProgressCtx.progress === "checkout"}
+        onClose={handleFinish}
+      >
+        <h2>Success!</h2>
+        <p>Your order was submitted successfully.</p>
+        <p>Check out your email for the order.</p>
+        <p>Thank you for choosing Meals Restaurant!</p>
+        <p className="modal-actions">
+          <Button onClick={handleFinish}>Okay</Button>
+        </p>
+      </Modal>
+    );
+  }
+
+  return (
+    <Modal
+      open={userProgressCtx.progress === "checkout"}
+      onClose={handleCloseCart}
+    >
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p>Total amount: {currencyFormatter.format(cartTotal)}</p>
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="E-mail Adress" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        {error && <Error title="failed to submit order" message={error} />}
+        <p className="modal-actions">{actions}</p>
+      </form>
+    </Modal>
+  );
+};
+
+export default Checkout;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+import { useContext } from "react";
+import UserProgressContext from "../store/UserProgressContext";
+
+const Header = () => {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src="/logo.jpg" alt="A restaurant" />
+        <h1>Meals Restaurant</h1>
+      </div>
+      <nav>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
